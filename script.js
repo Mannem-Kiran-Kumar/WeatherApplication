afcc51809b3006ebceddfb9f3345299faf7c6bf3
Kiran <kirankumar520260@gmail.com>
2025-07-07 19:23:32 +0530

    Fresh start

---
+++ script.js
@@ -0,0 +1,33 @@
+const apiKey = '3caabbdf703730aa74fcb7bcdd433ffe';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    } else {
+        alert("Please Enter a Location");
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert("Please Enter Correct Location");
+        });
+}
